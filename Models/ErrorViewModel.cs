357cfa3c694b25360a876d0753024524b951bb00
Ovais Mehboob Ahmed Khan <Ovais.Mehboob@microsoft.com>
2023-09-25 17:12:27 +0400

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace testmvcappgitops.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
